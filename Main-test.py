b2b142a4443bcdd9f8de900b63429a84222039b1
YoungSnook <161474744+YoungSnook@users.noreply.github.com>
2024-02-29 08:05:53 -0500

    Update Main-test.py

--- Main-test.py
+++ Main-test.py
@@ -1,7 +1,8 @@
-from Main import Add
+# Import the Add function, and assert that it works correctly.
+from main import Add
 
 def TestAdd():
-        assert Add(5,5) == 10
+        assert Add(2,3) == 5
         print("Add Function works correctly")
 
 if __name__ == '__main__':
